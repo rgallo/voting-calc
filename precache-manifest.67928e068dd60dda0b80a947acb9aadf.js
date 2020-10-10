25d6ba87cc05d4bce25ef2f34e8c1142dd044074
Ryan Gallo <ryan.j.gallo@gmail.com>
2020-10-09 21:39:25 -0400

    Updates

---
+++ precache-manifest.67928e068dd60dda0b80a947acb9aadf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bbd5adf8a2bb4f7dec66c9099f2aef",
+    "url": "/voting-calc/index.html"
+  },
+  {
+    "revision": "005c129e2ca986ae65f3",
+    "url": "/voting-calc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "104f660ccd0b8ec4cd10",
+    "url": "/voting-calc/static/js/2.badf955d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voting-calc/static/js/2.badf955d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005c129e2ca986ae65f3",
+    "url": "/voting-calc/static/js/main.bebaedc7.chunk.js"
+  },
+  {
+    "revision": "c605a7753a56cceaacd7",
+    "url": "/voting-calc/static/js/runtime-main.a396e6c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/voting-calc/static/media/logo.5d5d9eef.svg"
+  }
+]);
